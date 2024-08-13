10819697a60ff3d50bde32fe8dcc2bb43b9d8a57
SahilSharma2911 <sahilsharmarksh@gmail.com>
2024-08-13 09:54:48 +0530

    sdf

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <section className="font-sans bg-[#1B1714] lg:h-screen items-end justify-center w-full flex">
+    <section className="font-sans bg-[#1B1714] h-screen items-center lg:items-end justify-center w-full flex">
       <div className="flex flex-col lg:flex-row gap-10 md:gap-16 lg:gap-4 items-center justify-between  text-center lg:text-start px-[2rem] lg:px-[6rem] py-10 lg:mb-[4%] w-full">
         <div className="max-w-[820px] ">
           <h1 className="font-bold text-[30px] md:text-[50px] lg:text-[60px] leading-[39px] md:leading-[65px] lg:leading-[78px] px-[1.5rem] md:px-[4.5rem] lg:px-0 bg-gradient-to-r from-[#E9B873] via-[#FEDD8F] to-[#B78D51] text-transparent bg-clip-text">
@@ -21,7 +21,12 @@ const Header = () => {
           </span>
         </div>
         <div className="">
-          <Image src={"/Images/lock2.png"} width={250} height={250} className="w-[150px] md:w-[200px] lg:w-[250px]"/>
+          <Image
+            src={"/Images/lock2.png"}
+            width={250}
+            height={250}
+            className="w-[150px] md:w-[200px] lg:w-[250px]"
+          />
         </div>
       </div>
     </section>
